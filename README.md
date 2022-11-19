ca7d60a6d7b1e7e0fb933794484292e33372717f
woog2roid <woog2roid@gmail.com>
2022-11-20 00:32:36 +0900

    setup: HanJaeYeop Bee-Starter 설정

---
+++ README.md
@@ -0,0 +1,3 @@
+# [woog2roid.dev](https://woog2roid.dev)
+
+Powered by [Gatsby-starter-bee](https://github.com/JaeYeopHan/gatsby-starter-bee).
---
+++ gatsby-meta-config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  title: `Dev Blog(woog2roid)`,
+  description: `Blog posted about my development experiences`,
+  author: `woog2roid`,
+  introduction: `나의 발전하는 과정을 기록하기✒️`,
+  siteUrl: `https://woog2roid.dev`, // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: `woog2roid`, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: ``, // Your Facebook account
+    linkedin: ``, // Your LinkedIn account
+    instagram: ``, // Your Instagram account
+  },
+  icon: `content/assets/favicon.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    disqusShortName: '', // Your disqus-short-name. check disqus.com.
+    utterances: '', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: '',
+  },
+  share: {
+    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: '0', // Add your google analytics tranking ID
+  ad: '', // Add your google adsense publisherId `ca-pub-xxxxxxxxxx`
+
+}
