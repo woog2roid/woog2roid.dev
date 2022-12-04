60f54c2ee66c71dca16f6dc78f7936e52f828a86
woog2roid <woog2roid@gmail.com>
2022-12-05 01:24:39 +0900

    [FIX] className변경

--- src/components/post-container/index.jsx
+++ src/components/post-container/index.jsx
@@ -1,9 +1,5 @@
 import React from 'react'
-import * as Storage from '../../utils/storage'
 
 export const PostContainer = ({ html }) => (
-  <div
-    className={'markdown-body ' + Storage.getTheme() ? 'dark' : 'light'}
-    dangerouslySetInnerHTML={{ __html: html }}
-  />
+  <div className={'markdown-body'} dangerouslySetInnerHTML={{ __html: html }} />
 )
