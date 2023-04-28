d9e069e0b99e1b03ae41a98a99dbd30cffad18a3
woog2roid <woog2roid@gmail.com>
2023-04-28 14:48:33 +0900

    feat(thumbnail): 생성일자 및 카테고리 추가

--- src/components/thumbnail-item/index.jsx
+++ src/components/thumbnail-item/index.jsx
@@ -8,7 +8,16 @@ export const ThumbnailItem = ({ node }) => (
   <Link className={`thumbnail ${TARGET_CLASS}`} to={node.fields.slug}>
     <div key={node.fields.slug}>
       <h3>{node.frontmatter.title || node.fields.slug}</h3>
-      <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+      <p style={{ fontWeight: 'bold' }}>
+        <time className="thumbnail-date">
+          <span>{node.frontmatter.category} | </span>
+          {node.frontmatter.date}
+        </time>
+      </p>
+      <p
+        className="summary"
+        dangerouslySetInnerHTML={{ __html: node.excerpt }}
+      />
     </div>
   </Link>
 )
